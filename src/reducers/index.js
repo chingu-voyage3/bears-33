bc67664bf8f14a615acdc070458838a0d9e82e8b
mariana-martins <mariana_martins@id.uff.br>
2018-01-01 18:41:32 +1300

    Add empty reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,23 @@
 import { combineReducers } from 'redux';
 
+import AboutMeReducer from './reducer_aboutMe'
+import AdoptMeReducer from './reducer_adoptMe'
+import ContactUsReducer from './reducer_contactUs'
+import HowToHelpReducer from './reducer_howToHelp'
+import HuskiesForHireReducer from './reducer_huskiesForHire'
+import HuskyDayOutReducer from './reducer_huskyDayOut'
+import SurrenderAHuskyReducer from './reducer_surrenderAHusky'
+import WhatYouNeedToKnowReducer from './reducer_whatYouNeedToKnow'
 
 const rootReducer = combineReducers({
-    // Add json objects here.
+    aboutMeSection: AboutMeReducer,
+    adoptMeSection: AdoptMeReducer,
+    contactUsSection: ContactUsReducer,
+    howToHelpSection: HowToHelpReducer,
+    huskiesForHireSection: HuskiesForHireReducer,
+    huskyDayOutSection: HuskyDayOutReducer,
+    surrenderAHuskySection: SurrenderAHuskyReducer,
+    whatYouNeedToKnowSection: WhatYouNeedToKnowReducer,
 });
 
 export default rootReducer;
