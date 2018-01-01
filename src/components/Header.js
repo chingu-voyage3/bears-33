fa6384db773db936b4e73dfd915c2cf107cb32b3
mariana-martins <mariana_martins@id.uff.br>
2018-01-01 22:17:50 +1300

    Add jumbotron - requires to fix background image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Component } from 'react';
+import { Jumbotron, Container, Button } from 'reactstrap';
 
 export default class Header extends Component {
     render() {
         return (
-            <header></header>
+            <header>
+                <div>
+                    <Jumbotron fluid className="jumbotron d-flex align-items-center">
+                        <Container fluid>
+                            <h1 className="display-5">HUSKY RESCUE NZ CHARITABLE TRUST</h1>
+                            <p className="lead">Rescue, Desex, Rehabilitate, Retrain and Rehome of Siberian Huskies in NZ</p>
+                            <Button color="danger">Our Dogs</Button>{' '}
+                        </Container>
+                    </Jumbotron>
+                </div>
+            </header>
         );
     }
 }
+
