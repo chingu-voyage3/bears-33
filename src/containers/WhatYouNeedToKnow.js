28d5f42b28fa16b73cbab98715f60669e155da46
mariana-martins <mariana_martins@id.uff.br>
2018-01-03 14:56:18 +1300

    Add modal on What You Need to Know section

--- src/containers/WhatYouNeedToKnow.js
+++ src/containers/WhatYouNeedToKnow.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import { Container, Button } from 'reactstrap';
+import { Container, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import YouTube from 'react-youtube';
 
 
 class WhatYouNeedToKnow extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false,
+            nestedModal: false,
+            closeAll: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+        this.toggleNested = this.toggleNested.bind(this);
+        this.toggleAll = this.toggleAll.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    toggleNested() {
+        this.setState({
+            nestedModal: !this.state.nestedModal,
+            closeAll: false
+        });
+    }
+
+    toggleAll() {
+        this.setState({
+            nestedModal: !this.state.nestedModal,
+            closeAll: true
+        });
+    }
     render() {
         return (
             <section>
@@ -14,7 +46,7 @@ class WhatYouNeedToKnow extends Component {
                     <div class="row align-items-center">
                         <div className="col-md-4">
                             <p>{ this.props.data.content }</p>
-                            <Button color="danger">Learn More</Button>{' '}
+                            <Button color="danger" onClick={this.toggle}>Read More</Button>
                         </div>
                         <div className="col-md-8">
                             <div className="embed-responsive embed-responsive-16by9">
@@ -26,6 +58,40 @@ class WhatYouNeedToKnow extends Component {
                         </div>
                     </div>
                 </Container>
+                {/*Modal*/}
+                <div>
+                    <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                        <ModalHeader toggle={this.toggle}>Bad Points</ModalHeader>
+                        <ModalBody>
+                            <p>Really important to know and be prepared for!</p>
+                            <ol>
+                                {this.props.data.badPoints.map(function(text){
+                                    return <li>{text}</li>
+                                })}
+                            </ol>
+                            <br />
+                            <Button color="danger" onClick={this.toggleNested}>Go to Good Points</Button>
+                            <Modal isOpen={this.state.nestedModal} toggle={this.toggleNested} onClosed={this.state.closeAll ? this.toggle : undefined}>
+                                <ModalHeader>Good Points</ModalHeader>
+                                <ModalBody>
+                                    <p>What you probably already know</p>
+                                    <ol>
+                                        {this.props.data.goodPoints.map(function(text){
+                                            return <li>{text}</li>
+                                        })}
+                                    </ol>
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="danger" onClick={this.toggleNested}>Did you skip the Bad points? GO BACK!</Button>{' '}
+                                    <Button color="secondary" onClick={this.toggleAll}>All Done</Button>
+                                </ModalFooter>
+                            </Modal>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="secondary" onClick={this.toggle}>Done</Button>
+                        </ModalFooter>
+                    </Modal>
+                </div>
             </section>
         );
     }
--- src/reducers/reducer_whatYouNeedToKnow.js
+++ src/reducers/reducer_whatYouNeedToKnow.js
@@ -2,6 +2,83 @@ export default function() {
     return {
         content: "A free spirit, non-conforming, sometimes aloof, and at the same time gregarious and friendly... " +
         "you will love the Siberian Husky if you can accept the independent nature of this wonderful breed.",
-        details: ""
+        badPoints: [
+            "Not a one-man dog- any human will do- this may be seen as a lack of loyalty",
+
+            "He will not guard your home or property.",
+
+            "Strong desire to run. If he gets free he will run so far he will be lost, if not hit by a car or train, or shot by a farmer.",
+
+            "Cannot be relied on to return to you on command. " +
+            "He will decide whether or not to return for himself, knowing that you cannot catch him. " +
+            "Hence ON LEAD ONLY dog. We do NOT support offlead walking.",
+
+            "Too independent and strong willed generally to be a candidate for obedience training/work.",
+
+            "Keen and efficient hunter and killer. Cannot be trusted with cats, small dogs, small pets or livestock of any sort. " +
+            "Please note - huskies have been known to kill cats & small dogs, that they have lived happily with for many years, " +
+            "for no obvious reason. It is Your risk and not something we advise.",
+
+            "Like any dog- must be exercised to keep him fit and contented, but this must be done ON lead. " +
+            "5km minimum per day (when under 20 degrees Celsius). We also recommend joining your local sled dog club. " +
+            "You cant race in NZ unless it is under 13 degrees Celsius. " +
+            "This is a good guideline for people if mountain biking (Bikejouring) or hobby sledding / scootering with their dogs. " +
+            "Late nights and early mornings before the temperatures get up!",
+
+            "Can be very destructive, especially when young and/or if left alone for a long time.",
+
+            "Needs company, either human or canine, and is miserable without it. You can keep them singularly but you must spend time with them.",
+
+            "Needs a safely enclosed exercise area. Your garden must be fully fenced and secure. " +
+            "Six foot (1.8m) high fencing USUALLY enough. But many dogs will jump 6 foot  or dig under it. " +
+            "Take care he cannot dig his way out beneath it, and do not leave wheelie bins, " +
+            "bbq's etc near the fence or he may use them to get over the top. Keep the garden gate locked," +
+            "otherwise there is a risk that visitors, window cleaners etc may leave them open. " +
+            "Any window or gate left open by your kids, flatmate, visitor, \"tinder\" etc may be used to escape!",
+
+            "Your garden is unlikely to remain neat and tidy with a Sibe, rampaging happily within.",
+
+            "He needs correct feeding- cheap food does not bode well with this breed." +
+            "Especially in winter when they are using up a ton of energy. We recommend Royal Canin - Endurance 4800 and Trail.",
+
+            "Moults twice a year. The quantity of fur shed can surprise you, especially in spring when the winter coat is replaced by a shorter, " +
+            "thinner summer coat. This is called blowing their coat...and blow it does....everywhere.",
+
+            "You need an understanding and experienced veterinary surgeon. Sibes are sensitive to some drugs, " +
+            "particularly anaesthetics, sedatives and tranquillisers. This is due to their relatively low metabolic rate and lack of body fat. " +
+            "Also the bulk of their fur can lead vets to overestimate their weight and so overdose them. " +
+            "Sibes should always be weighed accurately beforehand to avoid this.",
+
+            "The veterinary books now list epilepsy as a medical issue in huskies. This is mainly in backyard bred animals" +
+            "as they are often bred before the parents show signs or it may occur due to lack of correct nutrients during" +
+            "gestation or a difficult birth etc. This can be very difficult to control if the dog is diagnosed under 1yo."
+        ],
+        goodPoints: [
+            "Friendly with people of all ages.",
+
+            "An honest dog- his body language and voice can be taken at face value- he says what he means.",
+
+            "He has no guarding instinct and will greet and kiss an intruder the same as any other visitor.",
+
+            "Gregarious- he likes company.",
+
+            "Youthful in outlook, an average length of life is 11 years of age, sometimes 16 or more.",
+
+            "Robust athletic constitution. Great for active people!",
+
+            "Good travellers, new sights and sounds do not upset them.",
+
+            "Intelligent and mischievous.",
+
+            "Easy going and forgiving.",
+
+            "Clean, little or no doggy smell. Some people allergic to dogs can tolerate Siberian Husky fur.",
+
+            "Straightforward to groom unless you have a long coat.",
+
+            "They do not require as much food for their size as many other breeds.",
+
+            "Get on well with other well-adjusted medium to large canines. However they will take up a challenge if offered."
+        ]
     }
 };
